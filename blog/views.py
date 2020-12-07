bc0d374182620d8b2db16bb05243d38997c6d941
Shabix <kmharouna@gmail.com>
2020-12-07 15:18:12 +0300

    Created database and ran migrations to Post model to db

--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author':'Lukyamuzi Shaban',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'December 05 , 2020'
-    },
-    {
-        'author':'Tana Shakira',
-        'title':'Blog Post 2',
-        'content':'Second post content',
-        'date_posted':'December 04 , 2020'
-    },
-    {
-        'author':'Nassembwa Shadiya',
-        'title':'Blog Post 3',
-        'content':'Third post content',
-        'date_posted':'December 03 , 2020'
-    },
-    {
-        'author':'Kakooza Ibrahim',
-        'title':'Blog Post 4',
-        'content':'Fourth post content',
-        'date_posted':'December 02 , 2020'
-    }
-]
+from . models import Post
 
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
