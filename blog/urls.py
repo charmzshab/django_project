a85cabab9d81dede4492ace6772ae3f211f4f71a
Shabix <kmharouna@gmail.com>
2020-12-17 13:06:00 +0300

    added logic for create & delete post functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from . views import PostListView,PostDetailView
+from . views import (PostListView,
+                    PostDetailView,
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView)
 from . import views
 
 urlpatterns = [
     # path('', views.home, name='blog-home'),
-    path('', PostListView.as_view(), name='blog-home'),# -> looks for views with <app>_<viewtype>.html[blog/post_list.html]
+    path('', PostListView.as_view(), name='blog-home'),# -> looks for views with <app>/<model>_<viewtype>.html[blog/post_list.html]
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'), # since create & update will share the same view,
+                                                                    #convention expects <app>/<model>_form.html
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),# convention -> <app>/<model>_confirm_delete.html
     path('about/', views.about, name='blog-about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # from django.http import HttpResponse
-from django.views.generic import ListView,DetailView
+from django.views.generic import (ListView,
+                                DetailView,
+                                CreateView,
+                                UpdateView,
+                                DeleteView)
 from . models import Post
 
 
@@ -18,6 +23,38 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+ 
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    # sets the author of the post when creating a new post
+    def form_valid(self,form): 
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # prevents users that are not authors of the post to update it
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+     # prevents users that are not authors of the post to update it
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
     
 
 def about(request):
