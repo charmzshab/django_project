1e48cbcf23454a5b3dcc8ea220a594a2bfad9b07
Shabix <kmharouna@gmail.com>
2020-12-18 15:05:06 +0300

    Implemented AWS S3 for file uploads

--- users/models.py
+++ users/models.py
@@ -10,20 +10,22 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        '''
-        resizes large image to a smaller dimension 
-        by using the PILLOW library
-        '''
+    # def save(self,*args,**kwargs):
+    #     '''
+    #     resizes large image to a smaller dimension 
+    #     by using the PILLOW library
+    #     '''
+        
+    #     super().save(*args,**kwargs)
+    #     # *args - positional arguments
+    #     # **kwargs - keyword arguments
 
-        super().save()
+    #     img = Image.open(self.image.path)
 
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300,300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
 
 
 
